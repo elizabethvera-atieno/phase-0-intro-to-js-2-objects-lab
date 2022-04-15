1c87cf6c8fce9f0b372501553433b70afd347736
elizabethvera-atieno <atienolizvera@gmail.com>
2022-04-15 22:16:39 +0300

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name : 'Vera',
+    streetAddress : '22 Kapita'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employees = {...employee}
+    employees[key] = value
+    return employees
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const Employees = employee
+    Employees[key] = value
+    return Employees
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    const newEmployees = employee
+    delete newEmployees[key]
+    return newEmployees
+}
